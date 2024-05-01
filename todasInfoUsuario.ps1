0fa01625e6328adfebc2c40d5f1fe1e10e10c81e
João Vitor Nogueira <jvnogueira2010@gmail.com>
2024-05-01 14:59:58 -0300

    Automação para reset de senha, buscar todas as info dos users e implementação da pesquisa de usuários por nome, email ou NIF em uma única váriavel sendo necessário apenas o número do NIF sem precisar do SS ou SN

---
+++ todasInfoUsuario.ps1
@@ -0,0 +1,15 @@
+﻿# Importar o módulo do Active Directory
+Import-Module ActiveDirectory
+
+# Substitua "pesquisar aqui" pelo endereço de e-mail ou nome do usuário que deseja pesquisar
+$userInfo = "rubia.dias@sesisp.org.br"
+
+# Substituir por “EmailAddress” ou “Name”
+$Usuario = Get-ADUser -Filter {EmailAddress -eq $userInfo} -Properties *
+
+# Verificar se o usuário foi encontrado
+if ($Usuario) {
+    Write-Output $Usuario
+} else {
+    Write-Output "Usuário não encontrado."
+}
