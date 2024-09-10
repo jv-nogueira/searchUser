4b5aa2c332db7df7ddd89f408b0064fa4ccc2cab
João Vitor Nogueira <joao.nogueira.3@sesisenaisp.org.br>
2024-09-10 18:16:12 -0300

    Implementado a busca por TC, além de SS e SN

--- searchUser-NomeEmailNIF.ps1
+++ searchUser-NomeEmailNIF.ps1
@@ -1,63 +1,126 @@
 ﻿# Importar o módulo do Active Directory
 Import-Module ActiveDirectory
 
-# Substituir pelo nome de usuário completo que deseja pesquisar
-$userInfo = "1079520"
+# Lista de NIFs que deseja pesquisar
+$userInfo = Read-Host "Digite o NIF, nome completo ou endereço de e-mail do usuário"   
 
 # Função para executar o script
 function ExecuteScript {
     param($property, $value)
+    $Usuarios = Get-ADUser -Filter {$property -eq $value} -Properties Office, SamAccountName, DisplayName, EmailAddress, Title, telephoneNumber, Department, physicalDeliveryOfficeName, Name, Manager
+    return $Usuarios
+}
 
-    # Propriedades que vão ser filtradas
-    $Usuarios = Get-ADUser -Filter {$property -eq $value} -Properties Office, SamAccountName, DisplayName, EmailAddress 
+# Função para processar cada NIF na lista
+function ProcessNIF {
+    param($nif)
+    $Usuarios = @()
+    
+    if ($nif -like "*@*") {
+        Write-Output "Pesquisando por endereço de e-mail: $nif"
+        $Usuarios = ExecuteScript -property "EmailAddress" -value $nif
+    } elseif ($nif -match "\s") {
+        Write-Output "Pesquisando por nome: $nif"
+        $Usuarios = ExecuteScript -property "DisplayName" -value $nif
+    } elseif ($nif -match "sn" -or $nif -match "ss") {
+        Write-Output "Pesquisando pelo NIF: $nif"
+        $Usuarios = ExecuteScript -property "SamAccountName" -value $nif
+    } elseif ($nif -match "^\d+$") {
+        $nifWithSN = "SN" + $nif
+        Write-Output "Adicionado 'SN' ao número do NIF: $nifWithSN"
+        $Usuarios = @($Usuarios + (ExecuteScript -property "SamAccountName" -value $nifWithSN))
 
-    # Verificar se o usuário foi encontrado
-    if ($Usuarios) {
-        foreach ($Usuario in $Usuarios) {
-            Write-Output ""
-            Write-Output @"
-        NIF: $($Usuario.SamAccountName)  
-        Nome completo: $($Usuario.DisplayName)
-        Unidade: $($Usuario.Office)
-        E-mail: $($Usuario.EmailAddress)
-"@
-        }
+        $nifWithSS = "SS" + $nif
+        Write-Output "Adicionado 'SS' ao número do NIF: $nifWithSS"
+        $Usuarios = @($Usuarios + (ExecuteScript -property "SamAccountName" -value $nifWithSS))
+
+        $nifWithTC = "TC" + $nif
+        Write-Output "Adicionado 'TC' ao número do NIF: $nifWithTC"
+        $Usuarios = @($Usuarios + (ExecuteScript -property "SamAccountName" -value $nifWithTC))
     } else {
-        Write-Output "Usuário não encontrado para '$value'."
+        Write-Output "Formato de NIF não reconhecido: $nif"
+        return @()
+    }
+    return $Usuarios
+}
+
+# Coletar todas as informações de usuários
+$allUsers = @{}
+foreach ($nif in $userInfo) {
+    $users = ProcessNIF -nif $nif
+    foreach ($user in $users) {
+        if ($user -and $user.SamAccountName -and !$allUsers.ContainsKey($user.SamAccountName)) {
+            $allUsers[$user.SamAccountName] = $user
+        }
     }
 }
 
-# Verifica se $userInfo contém "@" para procurar somente por EmailAddress
-if ($userInfo -like "*@*") {
-    Write-Output "Pesquisando por endereço de e-mail."
-    # Execute seu script aqui
-    ExecuteScript -property "EmailAddress" -value $userInfo
+# Verificar se algum usuário foi encontrado
+if ($allUsers.Count -eq 0) {
+    Write-Output "Nenhum usuário encontrado para os NIFs fornecidos."
+    exit
 }
-# Verifica se $userInfo não contém números e nem "@" para procurar por SamAccountName
-elseif ($userInfo -match "\s") {
-    Write-Output "Pesquisando por nome."
-    # Execute seu script aqui
-    ExecuteScript -property "SamAccountName" -value $userInfo
-}
-# Pesquisar o NIF contem "SS" ou "SN"
-elseif ($userInfo -match "sn" -or $userInfo -match "ss") {
-    Write-Output "Pesquisando pelo NIF."
-    # Execute seu script aqui
-    ExecuteScript -property "SamAccountName" -value $userInfo
-}
-# Pesquisar o NIF se conter apenas números
-elseif ($userInfo -match "^\d+$"){
-# Adiciona "SN" ao número do NIF e executa o script
-    $userInfoWithSN = "SN" + $userInfo
-    Write-Output "Adicionado 'SN' ao número do NIF: $userInfoWithSN"
-    # Execute seu script aqui usando $userInfoWithSN
-    ExecuteScript -property "SamAccountName" -value $userInfoWithSN
 
-    # Adiciona "SS" ao número do NIF e executa o script
-    $userInfoWithSS = "SS" + $userInfo
-    Write-Output "Adicionado 'SS' ao número do NIF: $userInfoWithSS"
-    # Execute seu script aqui usando $userInfoWithSS
-    ExecuteScript -property "SamAccountName" -value $userInfoWithSS
-}else{
-Write-Output "Nao encontrado."
+# Exibir informações dos usuários
+foreach ($Usuario in $allUsers.Values) {
+    $nomeCompleto = if ($Usuario.DisplayName) { $Usuario.DisplayName } else { $Usuario.Name }
+    $gerente = if ($Usuario.Manager -match "CN=([^,]+),") { $matches[1] } else { $Usuario.Manager }
+    $output = @()
+    
+    $output += "NIF: $($Usuario.SamAccountName)"
+    $output += "Nome completo: $nomeCompleto"
+    
+    if ($Usuario.Office -ne $Usuario.physicalDeliveryOfficeName) {
+        if ($Usuario.Office) {
+            $output += "Unidade: $($Usuario.Office)"
+        }
+        if ($Usuario.physicalDeliveryOfficeName) {
+            $output += "Escritório: $($Usuario.physicalDeliveryOfficeName)"
+        }
+    } elseif ($Usuario.Office) {
+        $output += "Unidade: $($Usuario.Office)"
+    }
+    
+    if ($Usuario.Department) {
+        $output += "Departamento: $($Usuario.Department)"
+    }
+
+    if ($gerente) {
+        $output += "Superior imediato: $gerente"
+    }
+    
+    if ($Usuario.Title) {
+        $output += "Cargo: $($Usuario.Title)"
+    }
+    
+    if ($Usuario.telephoneNumber) {
+        $output += "Telefone: $($Usuario.telephoneNumber)"
+    }
+    
+    if ($Usuario.EmailAddress) {
+        $output += "E-mail: 
+        $($Usuario.EmailAddress)"
+    }
+    
+    Write-Output ""
+    $output -join "`n"
+    Write-Output ""
+}
+
+# Perguntar se deseja redefinir a senha para todos os usuários
+$resetarSenha = Read-Host "Deseja resetar a senha de todos os usuários listados? Responda com S ou N"
+
+if ($resetarSenha -eq "S") {
+    foreach ($Usuario in $allUsers.Values) {
+        if ($Usuario.SamAccountName) {
+            $novaSenha = ConvertTo-SecureString -AsPlainText "Sesisenaisp@24" -Force
+            Set-ADAccountPassword -Identity $Usuario.SamAccountName -NewPassword $novaSenha -Reset
+            Set-ADUser -Identity $Usuario.SamAccountName -ChangePasswordAtLogon $true
+            Write-Output "Senha redefinida com sucesso para o usuário: $($Usuario.DisplayName)"
+        }
+    }
+} elseif ($resetarSenha -eq "N") {
+    Write-Output "Operação cancelada."
+} else {
+    Write-Output "Resposta inválida. Operação cancelada."
 }
