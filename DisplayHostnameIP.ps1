fa181eff0e442e8b47636912db9c53ca6d6cb22a
João Vitor Nogueira <joao.nogueira.3@sesisenaisp.org.br>
2024-12-13 15:49:57 -0300

    Implementa SN e winver

--- DisplayHostnameIP.ps1
+++ DisplayHostnameIP.ps1
@@ -1,10 +1,10 @@
 # Carrega o assembly do Windows Forms
 Add-Type -AssemblyName System.Windows.Forms
 
-# Função para obter informações da rede
+# FunÃ§Ã£o para obter informaÃ§Ãµes da rede
 function Get-NetworkInfo {
     $hostname = $env:COMPUTERNAME
-    # Obtém as informações de IP usando Get-NetIPConfiguration
+    # ObtÃ©m as informaÃ§Ãµes de IP usando Get-NetIPConfiguration
     $netInfo = Get-NetIPConfiguration | Where-Object { $_.IPv4Address -ne $null }
     $ipv4 = $netInfo.IPv4Address.IPAddress
     return [PSCustomObject]@{
@@ -13,23 +13,23 @@ function Get-NetworkInfo {
     }
 }
 
-# Criação do Formulário
+# Formulario
 $form = New-Object Windows.Forms.Form
-$form.Text = "Informações de Rede"
-$form.Size = New-Object Drawing.Size(350,200)
+$form.Text = "InformaÃ§Ãµes de Rede"
+$form.Size = New-Object Drawing.Size(350,240)
 $form.StartPosition = "CenterScreen"
 
-# Obtém as informações de rede
+# Informacoes de rede
 $networkInfo = Get-NetworkInfo
 
-# Criação do label para exibir o hostname
+# Label para exibir o hostname
 $labelHostname = New-Object Windows.Forms.Label
 $labelHostname.Size = New-Object Drawing.Size(200,30)
 $labelHostname.Location = New-Object Drawing.Point(25, 30)
 $labelHostname.Text = "Hostname: " + $networkInfo.Hostname
 $form.Controls.Add($labelHostname)
 
-# Criação do botão para copiar o hostname
+# Botao para copiar o hostname
 $btnCopyHostname = New-Object Windows.Forms.Button
 $btnCopyHostname.Size = New-Object Drawing.Size(100,30)
 $btnCopyHostname.Location = New-Object Drawing.Point(230, 30)
@@ -39,14 +39,14 @@ $btnCopyHostname.Add_Click({
 })
 $form.Controls.Add($btnCopyHostname)
 
-# Criação do label para exibir o IPv4
+# Label para exibir o IPv4
 $labelIPv4 = New-Object Windows.Forms.Label
 $labelIPv4.Size = New-Object Drawing.Size(200,30)
 $labelIPv4.Location = New-Object Drawing.Point(25, 70)
 $labelIPv4.Text = "IPv4: " + $networkInfo.IPv4
 $form.Controls.Add($labelIPv4)
 
-# Criação do botão para copiar o IPv4
+# Botao para copiar o IPv4
 $btnCopyIPv4 = New-Object Windows.Forms.Button
 $btnCopyIPv4.Size = New-Object Drawing.Size(100,30)
 $btnCopyIPv4.Location = New-Object Drawing.Point(230, 70)
@@ -56,5 +56,39 @@ $btnCopyIPv4.Add_Click({
 })
 $form.Controls.Add($btnCopyIPv4)
 
-# Exibe o formulário
+# Label para exibir SN
+$labelSN = New-Object Windows.Forms.Label
+$labelSN.Size = New-Object Drawing.Size(200,30)
+$labelSN.Location = New-Object Drawing.Point(25, 110)
+$labelSN.Text = "SN: " + (wmic bios get serialnumber).Trim()
+$form.Controls.Add($labelSN)
+
+# Botao para copiar SN
+$btnCopySN = New-Object Windows.Forms.Button
+$btnCopySN.Size = New-Object Drawing.Size(100,30)
+$btnCopySN.Location = New-Object Drawing.Point(230, 110)
+$btnCopySN.Text = "Copiar"
+$btnCopySN.Add_Click({
+    [System.Windows.Forms.Clipboard]::SetText((wmic bios get serialnumber).Trim())
+})
+$form.Controls.Add($btnCopySN)
+
+# Label para exibir winver
+$labelWinver = New-Object Windows.Forms.Label
+$labelWinver.Size = New-Object Drawing.Size(200,30)
+$labelWinver.Location = New-Object Drawing.Point(25, 150)
+$labelWinver.Text = "Versão do Windows: " + (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").DisplayVersion
+$form.Controls.Add($labelWinver)
+
+# Botao para copiar winver
+$btnCopyWinver = New-Object Windows.Forms.Button
+$btnCopyWinver.Size = New-Object Drawing.Size(100,30)
+$btnCopyWinver.Location = New-Object Drawing.Point(230, 150)
+$btnCopyWinver.Text = "Copiar"
+$btnCopyWinver.Add_Click({
+    [System.Windows.Forms.Clipboard]::SetText((Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion").DisplayVersion)
+})
+$form.Controls.Add($btnCopyWinver)
+
+# Exibe o formulÃ¡rio
 $form.ShowDialog()
